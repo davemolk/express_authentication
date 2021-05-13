cf576b043b70d0a43f2c47a3718936692040892d
davemolk <davemolk@gmail.com>
2021-05-13 13:10:28 -0600

    server: Import passport and pass through middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const passport = require("./config/ppConfig");
 
-router.get('/signup', (req, res) => {
-  res.render('auth/signup');
+router.get("/signup", (req, res) => {
+  res.render("auth/signup");
 });
 
-router.get('/login', (req, res) => {
-  res.render('auth/login');
+router.get("/login", (req, res) => {
+  res.render("auth/login");
 });
 
 module.exports = router;
