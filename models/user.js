ed112ba4e46923fdcee68588a1e2aac2b97a2197
davemolk <davemolk@gmail.com>
2021-05-13 10:59:37 -0600

    password encryption

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 "use strict";
+const bcrypt = require("bcrypt");
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class user extends Model {
@@ -46,5 +47,27 @@ module.exports = (sequelize, DataTypes) => {
       modelName: "user",
     }
   );
+
+  user.addHook("beforeCreate", (pendingUser) => {
+    // pendingUser is user object that gets passed to DB
+    let hash = bcrypt.hashSync(pendingUser.password, 12); // hash 12 times
+    pendingUser.password = hash; // this will go to the DB
+  });
+
+  // Check the password on Sign-In and compare it to the hashed password in the DB
+  user.prototype.validPassword = function (typedPassword) {
+    let isCorrectPassword = bcrypt.compareSync(typedPassword, this.password);
+    // check to see if password is correct.
+    return isCorrectPassword;
+  };
+
+  // return an object from the database of the user without the encrypted password
+  user.prototype.toJSON = function () {
+    let userData = this.get();
+    delete userData.password;
+    // it doesn't delete password from database, only removes it.
+    return userData;
+  };
+
   return user;
 };
