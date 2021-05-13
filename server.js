66e620aa32a90493cdcc5a112353d650aaa377a3
davemolk <davemolk@gmail.com>
2021-05-13 12:27:26 -0600

    Create env variable and add session and flash middleware

--- server.js
+++ server.js
@@ -1,25 +1,43 @@
-require('dotenv').config();
-const express = require('express');
-const layouts = require('express-ejs-layouts');
+require("dotenv").config();
+const express = require("express");
+const layouts = require("express-ejs-layouts");
 const app = express();
+const session = require("express-session");
+const flash = require("connect-flash");
 
-app.set('view engine', 'ejs');
+const SECRET_SESSION = process.env.SECRET_SESSION;
 
-app.use(require('morgan')('dev'));
+app.set("view engine", "ejs");
+
+app.use(require("morgan")("dev"));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 app.use(layouts);
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.use(
+  session({
+    secret: SECRET_SESSION, // What we actually will be giving the user on our site as a session cookie
+    resave: false, // Save the session even if it's modified, make this false
+    saveUninitialized: true, // If we have a new session, we save it, therefore making that true
+  })
+);
+app.use(flash());
+app.use((req, res, next) => {
+  console.log(res.locals);
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
 });
 
-app.get('/profile', (req, res) => {
-  res.render('profile');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.use('/auth', require('./controllers/auth'));
+app.get("/profile", (req, res) => {
+  res.render("profile");
+});
 
+app.use("/auth", require("./controllers/auth"));
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
