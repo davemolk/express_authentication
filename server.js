342c767bdf5860d3176809fecfb88f8435936814
Dave Molk <80996755+davemolk@users.noreply.github.com>
2021-05-18 20:41:23 -0600

    Update server.js

--- server.js
+++ server.js
@@ -38,12 +38,12 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.use("/auth", require("./controllers/auth"));
-
 app.get("/profile", isLoggedIn, (req, res) => {
   const { id, name, email } = req.user.get();
   res.render("profile", { id, name, email });
 });
+
+app.use("/auth", require("./controllers/auth"));
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
